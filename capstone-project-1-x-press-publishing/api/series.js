e3709d934b3c13a198406ec21761604bbf1398f7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-20 14:35:22 +0000

    Added param at series.js

--- capstone-project-1-x-press-publishing/api/series.js
+++ capstone-project-1-x-press-publishing/api/series.js
@@ -4,6 +4,24 @@ const seriesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+seriesRouter.param('seriesId', (req, res, next, seriesId) => {
+    const sql = 'SELECT * FROM Series WHERE Series.id = $seriesId';
+    const values = {$seriesId: seriesId};
+    db.get(sql, values, (error, series) => {
+        if (error) {
+            next(error);
+        } else if (series) {
+            req.series = series;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    })
+});
+
+
+
+
 seriesRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Series', (err, series) => {
         if (err) {
