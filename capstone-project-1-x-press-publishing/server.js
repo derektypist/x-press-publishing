8a02f72d19ce71da76d9c7ce0292dfff1c3659c1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-20 09:42:25 +0000

    Added listen method in server.js

--- capstone-project-1-x-press-publishing/server.js
+++ capstone-project-1-x-press-publishing/server.js
@@ -6,3 +6,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(bodyParser.json());
+app.use(cors());
+app.use(errorhandler());
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT ' + PORT);
+});
+
+module.exports = app;
