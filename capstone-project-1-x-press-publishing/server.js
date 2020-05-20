af068d9525703c90d2644df07f88a770b2a13664
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-20 09:36:25 +0000

    Added uses in server.js

--- capstone-project-1-x-press-publishing/server.js
+++ capstone-project-1-x-press-publishing/server.js
@@ -2,3 +2,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(bodyParser.json());
