c07f3cee444dc84351d49a430f1dee1c769f3b32
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-20 09:53:01 +0000

    Added apiRouter constant at server.js

--- capstone-project-1-x-press-publishing/server.js
+++ capstone-project-1-x-press-publishing/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
 
+const apiRouter = require('./api/api');
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(bodyParser.json());
