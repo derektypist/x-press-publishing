32c658550bd88abe2863b3c9b725f36429576d1d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-20 10:04:36 +0000

    Updated migration.js

--- capstone-project-1-x-press-publishing/server.js
+++ capstone-project-1-x-press-publishing/server.js
@@ -9,8 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorhandler());
 app.use(morgan('dev'));
+
+app.use('/api', apiRouter);
+
+app.use(errorhandler());
+
 
 app.listen(PORT, () => {
     console.log('Listening on PORT ' + PORT);
