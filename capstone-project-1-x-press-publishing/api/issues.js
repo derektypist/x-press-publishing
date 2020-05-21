3c9e5480b58434fa7a84b7f141520b4275b18d57
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-05-21 13:08:53 +0000

    Added post route at issues.js

--- capstone-project-1-x-press-publishing/api/issues.js
+++ capstone-project-1-x-press-publishing/api/issues.js
@@ -16,5 +16,49 @@ issuesRouter.get('/', (req, res, next) => {
     });
 });
 
+issuesRouter.post('/', (req, res, next) => {
+    const name = req.body.issue.name;
+    const issueNumber = req.body.issue.issueNumber;
+    const publicationDate = req.body.issue.publicationDate;
+    const artistId = req.body.issue.artistId;
+    const artistSql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
+    const artistValues = {$artistId: artistId};
+    db.get(artistSql, artistValues, (error, artist) => {
+        if (error) {
+            next(error);
+        } else {
+            if (!name || !issueNumber || !publicationDate || !artist) {
+                return res.sendStatus(400);
+            }
+
+            const sql = 'INSERT INTO ISSUE (name, issue_number, publication_date, artist_id, series_id) ' +
+            'VALUES ($name, $issueNumber, $publicationDate, $artistId, $seriesId)';
+            const values = {
+                $name: name,
+                $issueNumber: issueNumber,
+                $publicationDate: publicationDate,
+                $artistId: artistId,
+                $seriesId: req.params.seriesId
+            };
+
+            db.run(sql, values, function(error)  {
+                if (error) {
+                    next(error);
+                } else {
+                    db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`, (error, issue) => {
+                        res.status(201).json({issue: issue});
+                    });
+                }
+            });
+
+        }
+    });
+});
+
+
+
+
+
+
 
 module.exports = issuesRouter;
